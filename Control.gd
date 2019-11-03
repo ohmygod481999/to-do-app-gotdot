700f821c3c8cee2cf6c9540f59c3715308d1acaf
kuribohkute <kuribohwithwing@gmail.com>
2019-11-03 23:01:30 +0700

    add layout

--- Control.gd
+++ Control.gd
@@ -4,16 +4,60 @@ extends Control
 # var a = 2
 # var b = "text"
 export(NodePath) var button
+export (PackedScene) var ListItem
+
+var data = [
+	{
+		"id": 0,
+		"name": "List1",
+		"data": [
+			"data1",
+			"data2",
+			"data3"
+		]
+	},
+	{
+		"id": 1,
+		"name": "List2",
+		"data": [
+			"data4",
+			"data5",
+			"data6"
+		]
+	}
+]
+
+var listDone = [
+	"done1",
+	"done2"
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var collections = $Panel/LeftPanel/Body/ListCollection
+	for data_item in data:
+		collections.add_item(data_item["name"])
+	reset_actions(1)
 
+func delete_children(node):
+	for n in node.get_children():
+		node.remove_child(n)
+		n.queue_free()
 
+func reset_actions(id):
+	var action_panel = $Panel/RightPanel/List/ListAction
+	delete_children(action_panel)
+	for data_item in data:
+		if data_item["id"] == id:
+			for item in data_item["data"]:
+				var myItem = preload("res://ListItem.tscn").instance()
+				myItem.init(item)
+				action_panel.add_item(myItem)
+			break
 
 func _on_Button_pressed():
 	$Popup.popup()
 
 func _on_Button_2_pressed():
-	$Panel/ItemList.add_item($Popup/Panel/TextEdit.text)
+	$Panel/LeftPanel/Body/ListCollection.add_item($Popup/Panel/TextEdit.text)
 	$Popup.hide()
---
+++ DateTime.gd
@@ -0,0 +1,27 @@
+extends Label
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var time = OS.get_datetime()
+var dayofweek = time["day"]
+
+var map_date = {
+	0: "Sunday",
+	1: "Monday",
+	2: "Tuesday",
+	3: "Wednesday",
+	4: "Thursday",
+	5: "Friday",
+	6: "Sartuday"
+}
+
+func get_date_format():
+	return map_date[dayofweek] + ", " + str(time["day"]) + "/" + str(time["month"]) + "/" + str(time["year"])
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	text = get_date_format()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
